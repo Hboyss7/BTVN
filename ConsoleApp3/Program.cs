25915f375299c9c7087c48b6804d2b37c7f5c951
Hboyss7 <150256806+Hboyss7@users.noreply.github.com>
2024-08-19 14:46:15 +0700

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.IO;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket SockServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipadAny = IPAddress.Any;
+            IPEndPoint serverEndPoint = new IPEndPoint(ipadAny,6174);
+
+            SockServer.Bind(serverEndPoint);
+
+            SockServer.Listen(10);
+            Console.WriteLine("Waiting for client connect...");
+
+            Socket SockClient= SockServer.Accept();
+            Console.WriteLine("Client connected");
+
+            EndPoint clientEndPoint = SockClient.RemoteEndPoint;
+            Console.WriteLine(clientEndPoint.ToString());
+
+            byte[] buffer;
+
+            while (true)
+            {
+                string hello = Console.ReadLine();
+                if (hello == null)
+                {
+                    break;
+                }
+                buffer = Encoding.ASCII.GetBytes(hello);
+                SockClient.Send(buffer, 0, buffer.Length, SocketFlags.None);
+                buffer = new byte[1024];
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
